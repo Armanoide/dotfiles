9047d60b3e887ba8528c295e8effc755aae86a82
Norbert Billa <norbert.billa@gmail.com>
2025-12-01 03:03:30 +0100

    sketchybar: display all space at the start

--- .config/sketchybar/plugins/workspace_manager.sh
+++ .config/sketchybar/plugins/workspace_manager.sh
@@ -3,12 +3,7 @@
 CONFIG_DIR="${CONFIG_DIR:-$HOME/.config/sketchybar}"
 source "$CONFIG_DIR/colors.sh"
 
-# Get workspace states
-NON_EMPTY=($(/opt/homebrew/bin/aerospace list-workspaces --monitor focused --empty no))
-FOCUSED=$(/opt/homebrew/bin/aerospace list-workspaces --focused)
-
-# Combine workspaces to show (non-empty + focused)
-WORKSPACES_TO_SHOW=($(echo "${NON_EMPTY[@]}"$'\n'"$FOCUSED" | sort -un))
+WORKSPACES=(1 2 3 4 5 6 7 8 9)
 
 # Get existing space items from SketchyBar
 EXISTING=($(sketchybar --query bar | jq -r '.items[] | select(startswith("space."))'))
@@ -16,14 +11,10 @@ EXISTING=($(sketchybar --query bar | jq -r '.items[] | select(startswith("space.
 # Remove workspaces that shouldn't be shown
 for existing in "${EXISTING[@]}"; do
   sid=${existing#space.}
-  # check if workspace is empty and not focused
-  # if [[ ! " ${WORKSPACES_TO_SHOW[@]} " =~ " ${sid} " ]]; then
-  #   sketchybar --remove "$existing"
-  # fi
 done
 
 # Add/update workspaces that should be shown
-for sid in "${WORKSPACES_TO_SHOW[@]}"; do
+for sid in "${WORKSPACES[@]}"; do
   if ! sketchybar --query "space.$sid" &>/dev/null; then
     if [ $sid = "11" ]; then
       LABEL="magic"
