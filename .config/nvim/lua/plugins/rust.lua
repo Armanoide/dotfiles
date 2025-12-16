9ef9538d372d291c334fd19c20484534dc797808
Norbert Billa <norbert.billa@gmail.com>
2025-12-16 22:48:05 +0100

    nvim: cleanup rust plugins

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -25,68 +25,50 @@ return {
   },
   { "arzg/vim-rust-syntax-ext" },
 
-  -- Alternative Rust LSP (rustaceanvim)
   {
     "mrcjkb/rustaceanvim",
-    version = "^6", -- recommended
+    version = "^6",
     ft = "rust",
     dependencies = {
       "mason-org/mason.nvim",
       "mason-org/mason-lspconfig.nvim",
     },
-    config = function()
+    opts = {
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            cargo = { allFeatures = true },
+            diagnostics = { enable = true },
+            checkOnSave = { command = "clippy" },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      -- Setup Debugger Paths
       local codelldb_path = vim.fn.exepath("codelldb")
       local liblldb_path = vim.fn.expand("$MASON/packages/codelldb/extension/lldb/lib/liblldb.dylib")
 
-      local cfg = require("rustaceanvim.config")
+      -- If on Linux, change .dylib to .so
+      if vim.loop.os_uname().sysname ~= "Darwin" then
+        liblldb_path = vim.fn.expand("$MASON/packages/codelldb/extension/lldb/lib/liblldb.so")
+      end
 
-      vim.g.rustaceanvim = {
+      local cfg = require("rustaceanvim.config")
+      vim.g.rustaceanvim = vim.tbl_deep_extend("force", opts, {
         dap = {
           adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
         },
-        server = {
-          on_attach = function(_, bufnr)
-            local lsp_map = function(mode, keys, func, desc)
-              vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
-            end
-            -- rust-lsp mappings
-            lsp_map("n", "K", function()
-              vim.cmd.rustlsp({ "hover", "actions" })
-            end, "rust hover docs")
-            lsp_map("n", "<leader>cj", function()
-              vim.cmd.rustlsp("joinlines")
-            end, "rust join lines")
-            lsp_map("n", "<leader>cw", "<nop>", "rust commands")
-            lsp_map("n", "<leader>cwa", function()
-              vim.cmd.rustlsp("codeaction")
-            end, "rust code action")
-            lsp_map("n", "<leader>cwe", function()
-              vim.cmd.rustlsp("explainerror")
-            end, "rust error explain")
-            lsp_map("n", "<leader>cwd", function()
-              vim.cmd.rustlsp("opendocs")
-            end, "rust docs")
-            lsp_map("n", "<leader>cwm", function()
-              vim.cmd.rustlsp("expandmacro")
-            end, "rust expand macro")
-            -- copy from lsp_config
-            lsp_map("n", "gd", vim.lsp.buf.definition, "goto definition")
-            lsp_map("n", "gd", vim.lsp.buf.declaration, "goto declaration")
-            lsp_map("n", "gi", vim.lsp.buf.implementation, "goto implementation")
-            lsp_map("n", "go", vim.lsp.buf.type_definition, "goto type definition")
-            lsp_map("n", "gr", vim.lsp.buf.references, "goto references")
-            lsp_map("n", "ra", vim.lsp.buf.rename, "rename")
-          end,
-          settings = {
-            cargo = {
-              allfeatures = true,
-            },
-            ["rust-analyzer"] = {
-              diagnostics = { enable = true },
-            },
-          },
-        },
-      }
+      })
     end,
+    -- Custom Keymaps (Fixed the "Command not found" error by using keys)
+    keys = {
+      { "K", "<cmd>RustLsp { 'hover', 'actions' }<cr>", desc = "Rust Hover Docs" },
+      { "<leader>cj", "<cmd>RustLsp joinlines<cr>", desc = "Rust Join Lines" },
+      { "<leader>cwa", "<cmd>RustLsp codeAction<cr>", desc = "Rust Code Action" },
+      { "<leader>cwe", "<cmd>RustLsp explainError<cr>", desc = "Rust Error Explain" },
+      { "<leader>cwd", "<cmd>RustLsp openDocs<cr>", desc = "Rust Docs" },
+      { "<leader>cwm", "<cmd>RustLsp expandMacro<cr>", desc = "Rust Expand Macro" },
+    },
   },
 }
