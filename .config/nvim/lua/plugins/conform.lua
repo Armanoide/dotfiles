c5c7c876c73a506f39a33474d54e40e5b659ddd0
Norbert Billa <norbert.billa@gmail.com>
2026-01-15 06:09:20 +0100

    add autosave

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -11,6 +11,12 @@ return {
           rust = { "rustfmt" },
           javascript = { "prettierd", "prettier", stop_after_first = true },
           lua = { "stylua" },
+          prisma = { "lsp" },
+        },
+        -- Optional: This ensures it formats on save
+        format_on_save = {
+          lsp_fallback = true,
+          timeout_ms = 500,
         },
         log_level = vim.log.levels.ERROR,
       })
