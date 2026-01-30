e370456fda574eb56be5bcdf446de09eae1fb201
Norbert Billa <norbert.billa@gmail.com>
2026-01-30 23:17:32 +0100

    docker: immich start ia script

---
+++ docker/immich/start-ia.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker compose -f docker-compose.ia.yaml down                                                                   ✔  norbert@Mac-mini-de-Norbert  23:15:56  
+docker compose -f docker-compose.ia.yaml up -d
