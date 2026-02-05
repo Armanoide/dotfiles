0f349be1c2649aba8639375725e5e7a78f621942
UGREEN USER <norbert@debian.dedomain>
2026-02-05 22:17:33 +0100

    docker: update gitignore immich

--- docker/immich/start-ia.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker compose -f docker-compose.ia.yaml down                                                                   ✔  norbert@Mac-mini-de-Norbert  23:15:56  
-docker compose -f docker-compose.ia.yaml up -d
